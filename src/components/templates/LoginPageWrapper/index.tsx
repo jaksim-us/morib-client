d14c08e0ea6922216db6784164be6b0fd59971bc
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-11-17 23:14:55 +0900

    feat: 로그인 페이지 반응형 적용 (#215)

--- src/components/templates/LoginPageWrapper/index.tsx
+++ src/components/templates/LoginPageWrapper/index.tsx
@@ -1,23 +1,11 @@
 import { ReactNode } from 'react';
 
-import LoginBackground from '@/shared/assets/images/login_background.png';
-
 interface LoginPageWrapperProps {
 	children: ReactNode;
 }
 
 const LoginPageWrapper = ({ children }: LoginPageWrapperProps) => {
-	return (
-		<div
-			className={`h-[1080px] w-[1920px] pl-[64.5rem] pt-[36.1rem]`}
-			style={{
-				backgroundImage: `url(${LoginBackground})`,
-				backgroundSize: 'cover',
-			}}
-		>
-			{children}
-		</div>
-	);
+	return <div className="bg-login-bg flex h-screen items-center justify-center bg-cover">{children}</div>;
 };
 
 export default LoginPageWrapper;
