6a3fd3e2a1a7e8b81c672944a803e8692642b601
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-11-25 02:46:00 +0900

    refactor: 홈페이지 구조 absolute를 사용해서 변경 (#224)

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -166,10 +166,10 @@ const HomePage = () => {
 	}
 
 	return (
-		<div className="flex min-h-screen w-screen bg-gray-bg-01">
+		<div className="flex min-h-screen bg-gray-bg-01">
 			<Sidebar />
 
-			<div className={`absolute right-[4.4rem] top-[5.4rem] flex gap-[0.8rem] ${addTodayTodoOverlayStyle}`}>
+			<div className={`absolute right-[4.2rem] top-[5.4rem] flex gap-[0.8rem] ${addTodayTodoOverlayStyle}`}>
 				<button onClick={handleOpenFriendsModal}>
 					<FriendSettingIcon className="rounded-[1.6rem] hover:bg-gray-bg-04 active:bg-gray-bg-05" />
 				</button>
@@ -178,91 +178,83 @@ const HomePage = () => {
 				</button>
 			</div>
 
-			<div className="flex h-full w-full justify-between p-[4.2rem]">
-				<section>
-					<div className={addTodayTodoOverlayStyle}>
-						<div className="flex h-[11rem] items-center gap-[1.8rem] pt-[1.2rem]">
-							<ButtonUserProfile isMyProfile />
-							<ul className="flex gap-[1.8rem]">
-								<li>
-									<ButtonUserProfile isConnecting />
-								</li>
-								<li>
-									<ButtonUserProfile isConnecting />
-								</li>
-								<li>
-									<ButtonUserProfile isConnecting />
-								</li>
-							</ul>
-							<ButtonMoreFriends friendsCount={12} />
-						</div>
-						<DatePicker
-							todayDate={todayDate}
-							selectedDate={selectedDate}
-							onSelectedDateChange={handleSelectedDateChange}
-						/>
-					</div>
-					<div className="flex">
-						<article className="flex h-[732px] w-[1262px] gap-[2.8rem] overflow-x-auto">
-							{dailyCategoryTask.length !== 0 ? (
-								<>
-									{dailyCategoryTask.map(({ category, tasks }) => {
-										const { completedTasks, ongoingTasks } = splitTasksByCompletion(tasks);
-										return (
-											<BoxCategory
-												id={category.id}
-												key={category.id}
-												title={category.name}
-												ongoingTodos={ongoingTasks}
-												completedTodos={completedTasks}
-												updateTodayTodos={updateTodayTodos}
-												addingTodayTodoStatus={addingTodayTodoStatus}
-												getSelectedNumber={getSelectedNumber}
-												addingComplete={addingComplete}
-												onDeleteCategory={handleDeleteCategory}
-											/>
-										);
-									})}
-									{dailyCategoryTask.length <= 2 && (
-										<div className="flex flex-col">
-											<ButtonSVG className="flex-shrink-0" onClick={handleOpenCategoryModal}>
-												<LargePlusIcon className="rounded-full bg-gray-bg-03 hover:bg-gray-bg-05" />
-											</ButtonSVG>
-										</div>
-									)}
-								</>
-							) : (
-								<StatusDefaultHome onClick={handleOpenCategoryModal} />
-							)}
-						</article>
-						{dailyCategoryTask.length > 2 && (
-							<div className="ml-[2.2rem] flex flex-col">
-								<ButtonSVG className="flex-shrink-0" onClick={handleOpenCategoryModal}>
-									<LargePlusIcon className="rounded-full bg-gray-bg-03 hover:bg-gray-bg-05" />
-								</ButtonSVG>
-							</div>
+			<div className="absolute left-[11.6rem] top-[5.4rem] flex items-center gap-[1.8rem]">
+				<ButtonUserProfile isMyProfile />
+				<ul className="flex gap-[1.8rem]">
+					<li>
+						<ButtonUserProfile isConnecting />
+					</li>
+					<li>
+						<ButtonUserProfile isConnecting />
+					</li>
+					<li>
+						<ButtonUserProfile isConnecting />
+					</li>
+				</ul>
+				<ButtonMoreFriends friendsCount={12} />
+			</div>
+
+			<BoxTodayTodo
+				time={targetTime}
+				addingTodayTodoStatus={addingTodayTodoStatus}
+				selectedTodayTodos={todayTodos}
+				hasTodos={isTaskExist(dailyCategoryTask)}
+				enableAddingTodayTodo={enableAddingTodayTodo}
+				disableAddingTodayTodo={disableAddingTodayTodo}
+				deleteTodayTodos={deleteTodayTodos}
+				getSelectedNumber={getSelectedNumber}
+				enableComplete={enableComplete}
+				cancelComplte={cancelComplete}
+				addingComplete={addingComplete}
+				onCreateTodayTodos={handleCreateTodayTodos}
+			/>
+
+			<main className="absolute left-[11.6rem] top-[15.2rem] flex flex-col">
+				<DatePicker todayDate={todayDate} selectedDate={selectedDate} onSelectedDateChange={handleSelectedDateChange} />
+
+				<div className="flex">
+					<div className="flex h-[732px] w-[1262px] gap-[2.8rem] overflow-x-auto">
+						{dailyCategoryTask.length !== 0 ? (
+							<>
+								{dailyCategoryTask.map(({ category, tasks }) => {
+									const { completedTasks, ongoingTasks } = splitTasksByCompletion(tasks);
+									return (
+										<BoxCategory
+											id={category.id}
+											key={category.id}
+											title={category.name}
+											ongoingTodos={ongoingTasks}
+											completedTodos={completedTasks}
+											updateTodayTodos={updateTodayTodos}
+											addingTodayTodoStatus={addingTodayTodoStatus}
+											getSelectedNumber={getSelectedNumber}
+											addingComplete={addingComplete}
+											onDeleteCategory={handleDeleteCategory}
+										/>
+									);
+								})}
+								{dailyCategoryTask.length <= 2 && (
+									<div className="flex flex-col">
+										<ButtonSVG className="flex-shrink-0" onClick={handleOpenCategoryModal}>
+											<LargePlusIcon className="rounded-full bg-gray-bg-03 hover:bg-gray-bg-05" />
+										</ButtonSVG>
+									</div>
+								)}
+							</>
+						) : (
+							<StatusDefaultHome onClick={handleOpenCategoryModal} />
 						)}
 					</div>
-				</section>
-				<section className="absolute right-[4.2rem] top-[15.2rem]">
-					<div className="flex flex-col">
-						<BoxTodayTodo
-							time={targetTime}
-							addingTodayTodoStatus={addingTodayTodoStatus}
-							selectedTodayTodos={todayTodos}
-							hasTodos={isTaskExist(dailyCategoryTask)}
-							enableAddingTodayTodo={enableAddingTodayTodo}
-							disableAddingTodayTodo={disableAddingTodayTodo}
-							deleteTodayTodos={deleteTodayTodos}
-							getSelectedNumber={getSelectedNumber}
-							enableComplete={enableComplete}
-							cancelComplte={cancelComplete}
-							addingComplete={addingComplete}
-							onCreateTodayTodos={handleCreateTodayTodos}
-						/>
-					</div>
-				</section>
-			</div>
+					{dailyCategoryTask.length > 2 && (
+						<div className="ml-[2.2rem] flex flex-col">
+							<ButtonSVG className="flex-shrink-0" onClick={handleOpenCategoryModal}>
+								<LargePlusIcon className="rounded-full bg-gray-bg-03 hover:bg-gray-bg-05" />
+							</ButtonSVG>
+						</div>
+					)}
+				</div>
+			</main>
+
 			<ModalWrapper ref={categoryModalRef} backdrop={true}>
 				<ModalContentsCategory handleCloseModal={handleCloseModal} />
 			</ModalWrapper>
--- src/pages/HomePage/components/BoxTodayTodo.tsx
+++ src/pages/HomePage/components/BoxTodayTodo.tsx
@@ -1,5 +1,6 @@
-import { Task } from '@/shared/types/home';
 import { convertTime } from '@/shared/utils/time';
+
+import { Task } from '@/shared/types/home';
 
 import StatusAddBoxTodayTodo from './StatusAddBoxTodayTodo';
 import StatusDefaultBoxTodayTodo from './StatusDefaultBoxTodayTodo';
@@ -36,7 +37,7 @@ const BoxTodayTodo = ({
 	const { hours, minutes, seconds } = convertTime(time);
 
 	return (
-		<div className="flex h-[88.6rem] w-[40.2rem] flex-col rounded-[1.6rem] bg-gray-bg-03 p-[1.8rem]">
+		<div className="absolute right-[4.2rem] top-[15.2rem] flex h-5/6 w-[40.2rem] flex-col rounded-[1.6rem] bg-gray-bg-03 p-[1.8rem]">
 			<div className="rounded-[0.8rem] bg-gray-bg-05 pb-[1.8rem] pl-[2.6rem] pt-[2.2rem] text-white">
 				<p className="head-bold-24">오늘 나의 몰입 시간</p>
 				<p className="title-bold-32">{`${hours}시간 ${minutes}분 ${seconds}초`}</p>
