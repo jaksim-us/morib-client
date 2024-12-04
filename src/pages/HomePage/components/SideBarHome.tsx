0695981554d8235aabd7669345e7449d71c18448
ivoryeee <psa109@sookmyung.ac.kr>
2024-12-04 23:44:23 +0900

    [style] 사이드바 모립세트 뷰 추가 (#220)

--- src/pages/HomePage/components/SideBarHome.tsx
+++ src/pages/HomePage/components/SideBarHome.tsx
@@ -1,9 +1,13 @@
 import { useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import ModalWrapper, { ModalWrapperRef } from '@/shared/components/ModalWrapper';
 
 import GearIcon from '@/shared/assets/svgs/gear.svg?react';
 import LogoIcon from '@/shared/assets/svgs/logo_icon.svg?react';
+import MoribSetIcon from '@/shared/assets/svgs/moribSet.svg?react';
+
+import { ROUTES_CONFIG } from '@/router/routesConfig';
 
 import HomeSideBox from '@/components/atoms/HomeSideBox';
 import SpaceLogoBtn from '@/components/atoms/SpaceLogoBtn';
@@ -13,8 +17,13 @@ import { ModalContentsSetting } from './ModalContents';
 
 const SideBarHome = () => {
 	const modalRef = useRef<ModalWrapperRef>(null);
+	const navigate = useNavigate();
+
 	const openSettings = () => {
 		modalRef.current?.open();
+	};
+	const navigateMoribSet = () => {
+		navigate(ROUTES_CONFIG.moribSet.path);
 	};
 	return (
 		<HomeSideBox>
@@ -23,7 +32,10 @@ const SideBarHome = () => {
 					<LogoIcon />
 				</SpaceLogoBtn>
 			</div>
-			<div>
+			<div className="flex flex-col">
+				<ButtonSVG onClick={navigateMoribSet}>
+					<MoribSetIcon className="rounded-[1.6rem] hover:bg-gray-bg-04 active:bg-gray-bg-05" />
+				</ButtonSVG>
 				<ButtonSVG onClick={openSettings}>
 					<GearIcon className="rounded-[1.6rem] hover:bg-gray-bg-04 active:bg-gray-bg-05" />
 				</ButtonSVG>
