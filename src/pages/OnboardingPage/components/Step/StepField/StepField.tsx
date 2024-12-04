1f609c25183eaa098ac847dfaf3330b785d0184b
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-12-04 20:54:28 +0900

    style: StepField 컴포넌트 화면 너비에 따라 패딩값 일부 수정 (#224)

--- src/pages/OnboardingPage/components/Step/StepField/StepField.tsx
+++ src/pages/OnboardingPage/components/Step/StepField/StepField.tsx
@@ -11,24 +11,26 @@ interface StepFieldProps {
 
 const StepField = ({ setStep, onSelectField, selectedField, FIELDS }: StepFieldProps) => {
 	return (
-		<main className="flex min-h-screen w-full flex-col items-center justify-center">
+		<main className="flex min-h-screen w-full flex-col items-center overflow-auto pb-[18.2rem] pt-[18rem] 2xl:pb-0">
 			<h1 className="title-bold-36 mb-[2rem] text-center text-white">주로 어떤 분야에 집중하시나요?</h1>
 			<p className="body-reg-24 mb-[8.3rem] text-center text-gray-04">
 				업무 분야에 자주 쓰이는 서비스들을 추천 해드릴게요
 			</p>
 
-			<ul className="mb-[11.7rem] flex gap-x-[2rem]">
-				{FIELDS.map((field) => (
-					<li key={field}>
-						<button
-							onClick={() => onSelectField(field)}
-							className={`head-bold-24 flex h-[28rem] w-[22rem] items-center justify-center rounded-[8px] text-white ${selectedField.includes(field) ? 'border border-mint-01 bg-gray-bg-02' : 'bg-gray-bg-03'}`}
-						>
-							{field}
-						</button>
-					</li>
-				))}
-			</ul>
+			<div>
+				<ul className="mb-[11.7rem] flex flex-wrap justify-center gap-[2rem]">
+					{FIELDS.map((field) => (
+						<li key={field}>
+							<button
+								onClick={() => onSelectField(field)}
+								className={`head-bold-24 flex h-[28rem] w-[22rem] items-center justify-center rounded-[8px] text-white ${selectedField.includes(field) ? 'border border-mint-01 bg-gray-bg-02' : 'bg-gray-bg-03'}`}
+							>
+								{field}
+							</button>
+						</li>
+					))}
+				</ul>
+			</div>
 
 			<HomeLargeBtn
 				variant={HomeLargeBtnVariant.LARGE}
