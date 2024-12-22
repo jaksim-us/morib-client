7cf610b2b053cd306703f9e43ee53336ec23417d
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2024-12-23 00:13:14 +0900

    [Refactor] home 페이지 폴더구조 변경 (#231)
    * feat: home 페이지 폴더구조 변경 (#230)
    
    * feat: 공통적으로 쓰이는 BoxTodo, ButtonTogoToggle -> shared로 이동 (#230)
    
    * feat: shared로 이동한 컴포넌트 경로 변경 (#230)
    
    * refactor: ModalContents 폴더 구조 변경 (#230)
    
    * feat: import 문 구분을 위해 폴더 구조 배럴파일 구조로 변경 (#230)
    
    * feat: commit 누락된 부분 저장 (#230)
    
    * feat: 홈페이지 import 문 수정 (#230)
    
    * refactor: atoms 컴포넌트 용도에 맞게 폴더 이동 및 네이밍 컨벤션에 맞게 변경 (#230)
    
    * refactor: 바뀐 컴포넌트 경로에 맞게 import문 수정 (#230)
    
    * refactor: 홈에서만 쓰이는 hook 폴더 이동 및 훅을 사용하는 컴포넌트의 import문 변경 (#230)
    
    * refactor: ButtonSVG 사용 불필요하여 삭제 (#230)
    
    * refactor: 상아 피드백 반영해서 UI 분류 페이지 제거 모든 컴포넌트 depth 1로 고정 (#230)
    
    * refactor: hook import 경로 수정 (#230)

---
+++ src/pages/HomePage/BoxCategory/StatusDefaultBoxCategory/StatusDefaultBoxCategory.tsx
@@ -0,0 +1,12 @@
+const StatusDefaultBoxCategory = () => {
+	return (
+		<div className="flex justify-center text-gray-05">
+			<p className="mt-[27.2rem] text-center subhead-bold-22">
+				할 일을 추가하려면
+				<br />+ 아이콘을 선택해주세요.
+			</p>
+		</div>
+	);
+};
+
+export default StatusDefaultBoxCategory;
