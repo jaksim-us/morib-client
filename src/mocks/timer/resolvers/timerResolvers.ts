3c55d1cfd13ca2a70ab57acb5ff0f33e5c55dbfc
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-01-05 02:48:59 +0900

    타이머 mock  API 응답값, resolver 작성 (#243)

---
+++ src/mocks/timer/resolvers/timerResolvers.ts
@@ -0,0 +1,36 @@
+import { HttpResponse, http } from 'msw';
+
+import { TIMER_RES } from '../responses/timerResponses';
+
+export const TIMER_MOCK_URL = {
+	POST_TIMER_STOP: 'api/v2/timer/stop/:taskId',
+	GET_TODO_CARD: 'api/v2/timer/todo-card',
+};
+
+export const timerResolvers = [
+	http.post<{ taskId: string }, { targetDate: string; elapsedTime: number }>(
+		TIMER_MOCK_URL.POST_TIMER_STOP,
+		async ({ params, request }) => {
+			const { taskId } = params;
+			const { targetDate, elapsedTime } = await request.json();
+
+			if (!taskId || !targetDate || !elapsedTime) {
+				console.error('taskId or targetDate or elapsedTime is not exist');
+				throw new HttpResponse(null, { status: 400 });
+			}
+
+			return HttpResponse.json(TIMER_RES.POST_TIMER_STOP);
+		},
+	),
+
+	http.get(TIMER_MOCK_URL.GET_TODO_CARD, async ({ params }) => {
+		const { targetDate } = params;
+
+		if (!targetDate) {
+			console.error('targetDate is not exist');
+			throw new HttpResponse(null, { status: 400 });
+		}
+
+		return HttpResponse.json(TIMER_RES.GET_TODO_CARD);
+	}),
+];
