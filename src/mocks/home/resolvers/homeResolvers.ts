b8ad72aa59ac403a509acd3961073a7b6f064707
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-01-04 16:24:07 +0900

    [Feat] MSW 초기 세팅 및 예시 작성 (#241)
    * feat: msw 설치 (#240)
    
    * feat: msw worker script 저장 및 개발 환경에서만 msw가 실행되도록 적용 (#240)
    
    * feat: 예시 mocking 코드 작성 (#240)
    
    * feat: mock api를 이용한 axios 함수 예시 작성 (#240)
    
    * refactor: 타입 잘못 작성한 부분 수정 (#240)
    
    * feat: 경로 파라미터 타입 체크 누락된 부분 추가 (#240)

---
+++ src/mocks/home/resolvers/homeResolvers.ts
@@ -0,0 +1,75 @@
+import { HttpResponse, http } from 'msw';
+
+import { HOME_RES } from '../responses/homeResponses';
+
+export const HOME_MOCK_URL = {
+	GET_CATEGORY: 'api/v2/home',
+	GET_WORK_TIME: 'api/v2/timer',
+	POST_TIMER_START: 'api/v2/timer/start',
+	POST_CREATE_TASK: '/api/v2/tasks/:categoryId',
+	POST_TOGGLE_TASK_STATUS: 'api/v2/tasks/:taskId/status',
+};
+
+// LINK: https://mswjs.io/docs/best-practices/typescript/#request-handlers
+// NOTE: http.method<Params의 타입, RequestBody의 타입, ResponseBody의 타입, path의 타입(api 경로)>
+
+export const homeResolvers = [
+	http.get(HOME_MOCK_URL.GET_CATEGORY, async ({ request }) => {
+		const url = new URL(request.url);
+		const startDate = url.searchParams.get('startDate');
+		const endDate = url.searchParams.get('endDate');
+
+		if (!startDate || !endDate) {
+			throw new HttpResponse(null, { status: 400 });
+		}
+
+		return HttpResponse.json(HOME_RES.GET_CATEGORY);
+	}),
+
+	http.get(HOME_MOCK_URL.GET_WORK_TIME, async ({ params }) => {
+		const { targetDate } = params;
+
+		if (!targetDate) {
+			throw new HttpResponse(null, { status: 400 });
+		}
+
+		return HttpResponse.json(HOME_RES.GET_WORK_TIME);
+	}),
+
+	http.post<never, { taskList: number[] }>(HOME_MOCK_URL.POST_TIMER_START, async ({ request }) => {
+		const url = new URL(request.url);
+		const targetDate = url.searchParams.get('targetDate');
+
+		const { taskList } = await request.json();
+
+		if (!targetDate || !taskList) {
+			throw new HttpResponse(null, { status: 400 });
+		}
+
+		return HttpResponse.json(HOME_RES.POST_TIMER_START);
+	}),
+
+	http.post<{ categoryId: string }, { name: string; startDate: string; endDate: string }>(
+		HOME_MOCK_URL.POST_CREATE_TASK,
+		async ({ params, request }) => {
+			const { categoryId } = params;
+			const { name, startDate, endDate } = await request.json();
+
+			if (!categoryId || !name || !startDate || !endDate) {
+				throw new HttpResponse(null, { status: 400 });
+			}
+
+			return HttpResponse.json(HOME_RES.POST_CREATE_TASK);
+		},
+	),
+
+	http.post<{ taskId: string }>(HOME_MOCK_URL.POST_TOGGLE_TASK_STATUS, async ({ params }) => {
+		const { taskId } = params;
+
+		if (!taskId) {
+			throw new HttpResponse(null, { status: 400 });
+		}
+
+		return HttpResponse.json(HOME_RES.POST_TOGGLE_TASK_STATUS);
+	}),
+];
---
+++ src/mocks/home/responses/homeResponses.ts
@@ -0,0 +1,247 @@
+export const HOME_RES = {
+	GET_CATEGORY: {
+		status: 200,
+		message: '요청이 성공했습니다.',
+		data: [
+			{
+				date: '2024-07-01',
+				categories: [
+					{
+						category: {
+							id: 2,
+							name: 'BA 기본',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 1,
+							name: 'BA 기초',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 3,
+							name: 'BA 심화',
+						},
+						tasks: [],
+					},
+				],
+			},
+			{
+				date: '2024-07-02',
+				categories: [
+					{
+						category: {
+							id: 2,
+							name: 'BA 기본',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 1,
+							name: 'BA 기초',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 3,
+							name: 'BA 심화',
+						},
+						tasks: [],
+					},
+				],
+			},
+			{
+				date: '2024-07-03',
+				categories: [
+					{
+						category: {
+							id: 2,
+							name: 'BA 기본',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 1,
+							name: 'BA 기초',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 3,
+							name: 'BA 심화',
+						},
+						tasks: [],
+					},
+				],
+			},
+			{
+				date: '2024-07-04',
+				categories: [
+					{
+						category: {
+							id: 2,
+							name: 'BA 기본',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 1,
+							name: 'BA 기초',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 3,
+							name: 'BA 심화',
+						},
+						tasks: [
+							{
+								id: 7,
+								name: 'Random Forest 모델 구성',
+								startDate: '2024-07-04',
+								endDate: '2024-09-15',
+								elapsedTime: 0,
+								isComplete: true,
+							},
+						],
+					},
+				],
+			},
+			{
+				date: '2024-07-05',
+				categories: [
+					{
+						category: {
+							id: 2,
+							name: 'BA 기본',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 1,
+							name: 'BA 기초',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 3,
+							name: 'BA 심화',
+						},
+						tasks: [
+							{
+								id: 7,
+								name: 'Random Forest 모델 구성',
+								startDate: '2024-07-04',
+								endDate: '2024-09-15',
+								elapsedTime: 0,
+								isComplete: true,
+							},
+						],
+					},
+				],
+			},
+			{
+				date: '2024-07-06',
+				categories: [
+					{
+						category: {
+							id: 2,
+							name: 'BA 기본',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 1,
+							name: 'BA 기초',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 3,
+							name: 'BA 심화',
+						},
+						tasks: [
+							{
+								id: 7,
+								name: 'Random Forest 모델 구성',
+								startDate: '2024-07-04',
+								endDate: '2024-09-15',
+								elapsedTime: 0,
+								isComplete: true,
+							},
+						],
+					},
+				],
+			},
+			{
+				date: '2024-07-07',
+				categories: [
+					{
+						category: {
+							id: 2,
+							name: 'BA 기본',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 1,
+							name: 'BA 기초',
+						},
+						tasks: [],
+					},
+					{
+						category: {
+							id: 3,
+							name: 'BA 심화',
+						},
+						tasks: [
+							{
+								id: 7,
+								name: 'Random Forest 모델 구성',
+								startDate: '2024-07-04',
+								endDate: '2024-09-15',
+								elapsedTime: 0,
+								isComplete: true,
+							},
+						],
+					},
+				],
+			},
+		],
+	},
+	GET_WORK_TIME: {
+		status: 200,
+		message: '요청이 성공했습니다.',
+		data: {
+			targetDate: '2024-07-07',
+			sumTodayElapsedTime: 2000,
+		},
+	},
+	POST_TIMER_START: {
+		status: 200,
+		message: '요청이 성공했습니다.',
+	},
+	POST_CREATE_TASK: {
+		status: 200,
+		code: 's2000',
+		message: '요청이 성공했습니다.',
+	},
+	POST_TOGGLE_TASK_STATUS: {
+		status: 200,
+		message: '요청이 성공했습니다.',
+	},
+};
