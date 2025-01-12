07ad6613b4b01af72a42ae5d2ea4ca5379a14044
KIMGEONHWI <dirnrlarjsgn@naver.com>
2025-01-13 02:55:51 +0900

    feat: usePostTimerStop 함수 쿼리 생성 (#243)

--- src/shared/apisV2/timer/queries/index.ts
+++ src/shared/apisV2/timer/queries/index.ts
@@ -1,10 +1,17 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
-import { getTodoCard } from '../axios';
+import { getTodoCard, postTimerStop } from '../axios';
 
 export const useGetTodoCard = (targetDate: string) => {
 	return useQuery({
 		queryKey: ['todo', targetDate],
 		queryFn: () => getTodoCard(targetDate),
+	});
+};
+
+export const usePostTimerStop = () => {
+	return useMutation({
+		mutationFn: ({ taskId, elapsedTime, targetDate }: { taskId: string; elapsedTime: number; targetDate: string }) =>
+			postTimerStop(taskId, targetDate, elapsedTime),
 	});
 };
