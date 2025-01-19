3e5949be410dee39f6faec5073509b6724e886d7
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-01-20 02:26:55 +0900

    feat: 분야 선택 스탭, 허용 서비스 선택 스탭 api 로직 추가 (#249)

--- src/pages/OnboardingPage/StepField/StepField.tsx
+++ src/pages/OnboardingPage/StepField/StepField.tsx
@@ -1,15 +1,17 @@
 import HomeLargeBtn from '@/shared/components/ButtonHomeLarge/ButtonHomeLarge';
 
+import type { FieldType } from '@/shared/types/fileds';
 import { HomeLargeBtnVariant } from '@/shared/types/global';
+
+import { FIELDS } from '@/shared/constants/fields';
 
 interface StepFieldProps {
 	setStep: (step: string) => void;
-	onSelectField: (field: string) => void;
-	selectedField: string[];
-	FIELDS: string[];
+	onSelectField: (field: FieldType) => void;
+	selectedField: FieldType | null;
 }
 
-const StepField = ({ setStep, onSelectField, selectedField, FIELDS }: StepFieldProps) => {
+const StepField = ({ setStep, onSelectField, selectedField }: StepFieldProps) => {
 	return (
 		<main className="flex min-h-screen w-full flex-col items-center overflow-auto pb-[18.2rem] pt-[18rem] 2xl:pb-0">
 			<h1 className="mb-[2rem] text-center text-white title-bold-36">주로 어떤 분야에 집중하시나요?</h1>
@@ -19,11 +21,11 @@ const StepField = ({ setStep, onSelectField, selectedField, FIELDS }: StepFieldP
 
 			<div>
 				<ul className="mb-[11.7rem] flex flex-wrap justify-center gap-[2rem]">
-					{FIELDS.map((field) => (
+					{FIELDS.filter((field) => field !== '기타').map((field) => (
 						<li key={field}>
 							<button
 								onClick={() => onSelectField(field)}
-								className={`flex h-[28rem] w-[22rem] items-center justify-center rounded-[8px] text-white head-bold-24 ${selectedField.includes(field) ? 'border border-mint-01 bg-gray-bg-02' : 'bg-gray-bg-03'}`}
+								className={`flex h-[28rem] w-[22rem] items-center justify-center rounded-[8px] text-white head-bold-24 ${selectedField === field ? 'border border-mint-01 bg-gray-bg-02' : 'bg-gray-bg-03'}`}
 							>
 								{field}
 							</button>
@@ -35,7 +37,7 @@ const StepField = ({ setStep, onSelectField, selectedField, FIELDS }: StepFieldP
 			<HomeLargeBtn
 				variant={HomeLargeBtnVariant.LARGE}
 				onClick={() => setStep('service')}
-				disabled={selectedField.length === 0}
+				disabled={selectedField === null}
 				className="mb-[2rem]"
 			>
 				다음으로 넘어가기
