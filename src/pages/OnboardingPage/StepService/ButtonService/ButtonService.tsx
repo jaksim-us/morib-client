a94220768545dc0775499354f80801e9c974f295
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-01-20 02:24:53 +0900

    feat: 분야 별로 제공하는 서비스를 선택할 수 있는 ButtonService 컴포넌트 로직 추가 (#249)

--- src/pages/OnboardingPage/StepService/ButtonService/ButtonService.tsx
+++ src/pages/OnboardingPage/StepService/ButtonService/ButtonService.tsx
@@ -1,19 +1,21 @@
-import DesignIcon from '@/shared/assets/svgs/ic_service_design.svg?react';
+import { getServiceFavicon } from '../../utils/serviceUrl';
 
 interface ButtonServiceProps {
+	title: string;
+	url: string;
 	onAddSelectedService: (service: string) => void;
 }
 
-const ButtonService = ({ onAddSelectedService }: ButtonServiceProps) => {
+const ButtonService = ({ title, url, onAddSelectedService }: ButtonServiceProps) => {
 	return (
 		<button
-			onClick={() => onAddSelectedService('https://www.behance.net/')}
+			onClick={() => onAddSelectedService(url)}
 			className="flex h-[11rem] w-[34rem] items-center gap-x-[2rem] rounded-[8px] bg-gray-bg-03 p-[2rem]"
 		>
-			<DesignIcon />
+			<img src={getServiceFavicon(url)} alt={`${title} 아이콘`} className="h-[7rem] w-[7rem]" />
 			<div className="flex flex-col">
-				<p className="w-[21.3rem] truncate text-start text-white head-bold-24">디자인</p>
-				<p className="w-[21.3rem] truncate text-start text-gray-03 detail-reg-14">https://www.behance.net/</p>
+				<p className="w-[21.3rem] truncate text-start text-white head-bold-24">{title}</p>
+				<p className="w-[21.3rem] truncate text-start text-gray-03 detail-reg-14">{url}</p>
 			</div>
 		</button>
 	);
