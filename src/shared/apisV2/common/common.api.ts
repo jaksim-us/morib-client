057010a848b41628fe572b387eff25588d7804de
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-01 18:48:10 +0900

    feat: 온보딩에서 사용하는 api 함수 실제 url로 연동

--- src/shared/apisV2/common/common.api.ts
+++ src/shared/apisV2/common/common.api.ts
@@ -1,10 +1,12 @@
-import axios from 'axios';
-
 import { GetUrlNameReq, GetUrlNameRes } from '@/shared/types/api/common';
 
-import { COMMON_MOCK_URL } from '@/mocks/common/common.resolvers';
+import { authClient } from '../client';
+
+export const COMMON_MOCK_URL = {
+	GET_URL_NAME: 'api/v2/tabName',
+};
 
 export const getUrlName = async ({ url }: GetUrlNameReq): Promise<GetUrlNameRes> => {
-	const { data } = await axios.get(`${COMMON_MOCK_URL.GET_URL_NAME}?tabName=${url}`);
+	const { data } = await authClient.get(COMMON_MOCK_URL.GET_URL_NAME, { params: { url } });
 	return data;
 };
