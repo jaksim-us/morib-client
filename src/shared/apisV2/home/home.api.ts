b9a620125a130f6da9ba732c3f6ef61a60522c0e
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-02 19:17:14 +0900

    feat: 홈페이지 V2 api 연동

--- src/shared/apisV2/home/home.api.ts
+++ src/shared/apisV2/home/home.api.ts
@@ -1,5 +1,3 @@
-import axios from 'axios';
-
 import {
 	DeleteCategoryReq,
 	GetCategoryTaskReq,
@@ -11,30 +9,39 @@ import {
 	postAddTodayTodosReq,
 } from '@/shared/types/api/home';
 
-import { HOME_MOCK_URL } from '@/mocks/home/home.resolvers';
+import { authClient } from '../client';
+
+export const HOME_ENDPOINT = {
+	GET_CATEGORY: 'api/v2/home',
+	GET_WORK_TIME: 'api/v2/timer',
+	POST_TIMER_START: 'api/v2/timer/start',
+	POST_CREATE_TASK: '/api/v2/tasks/:categoryId',
+	POST_TOGGLE_TASK_STATUS: 'api/v2/tasks/:taskId/status',
+	DELETE_CATEGORY: 'api/v2/categories/:categoryId',
+};
 
 export const getCategoryTask = async ({ startDate, endDate }: GetCategoryTaskReq): Promise<GetCategoryTaskRes> => {
-	const { data } = await axios.get(HOME_MOCK_URL.GET_CATEGORY, {
+	const { data } = await authClient.get(HOME_ENDPOINT.GET_CATEGORY, {
 		params: { startDate, endDate },
 	});
 	return data;
 };
 
 export const getWorkTime = async ({ targetDate }: GetWorkTimeReq): Promise<GetWorkTimeRes> => {
-	const { data } = await axios.get(HOME_MOCK_URL.GET_WORK_TIME, {
+	const { data } = await authClient.get(HOME_ENDPOINT.GET_WORK_TIME, {
 		params: { targetDate },
 	});
 	return data;
 };
 
 export const postAddTodayTodos = async ({ targetDate, taskList }: postAddTodayTodosReq) => {
-	const { data } = await axios.post(HOME_MOCK_URL.POST_TIMER_START, { taskList }, { params: { targetDate } });
+	const { data } = await authClient.post(HOME_ENDPOINT.POST_TIMER_START, { taskList }, { params: { targetDate } });
 	return data;
 };
 
 export const postCreateTask = async ({ categoryId, name, startDate, endDate }: PostCreateTaskReq) => {
-	const { data } = await axios.post(
-		HOME_MOCK_URL.POST_CREATE_TASK.replace(':categoryId', String(categoryId)),
+	const { data } = await authClient.post(
+		HOME_ENDPOINT.POST_CREATE_TASK.replace(':categoryId', String(categoryId)),
 		{ name },
 		{ params: { startDate, endDate } },
 	);
@@ -43,11 +50,11 @@ export const postCreateTask = async ({ categoryId, name, startDate, endDate }: P
 };
 
 export const postToggleTaskStatus = async ({ taskId }: PostToggleTaskStatusReq) => {
-	const { data } = await axios.post(HOME_MOCK_URL.POST_TOGGLE_TASK_STATUS.replace(':taskId', String(taskId)));
+	const { data } = await authClient.post(HOME_ENDPOINT.POST_TOGGLE_TASK_STATUS.replace(':taskId', String(taskId)));
 	return data;
 };
 
 export const deleteCategory = async ({ categoryId }: DeleteCategoryReq) => {
-	const { data } = await axios.delete(HOME_MOCK_URL.DELETE_CATEGORY.replace(':categoryId', String(categoryId)));
+	const { data } = await authClient.delete(HOME_ENDPOINT.DELETE_CATEGORY.replace(':categoryId', String(categoryId)));
 	return data;
 };
