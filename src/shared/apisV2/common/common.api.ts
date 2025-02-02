54dfabdab244d4d2b01969701075e674d745af63
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-02 19:17:50 +0900

    feat: 의미를 명확히 하기 위해서 URL 대신 ENDPOINT로 네이밍 변경

--- src/shared/apisV2/common/common.api.ts
+++ src/shared/apisV2/common/common.api.ts
@@ -2,11 +2,11 @@ import { GetUrlNameReq, GetUrlNameRes } from '@/shared/types/api/common';
 
 import { authClient } from '../client';
 
-export const COMMON_MOCK_URL = {
+export const COMMON_ENDPOINT = {
 	GET_URL_NAME: 'api/v2/tabName',
 };
 
 export const getUrlName = async ({ url }: GetUrlNameReq): Promise<GetUrlNameRes> => {
-	const { data } = await authClient.get(COMMON_MOCK_URL.GET_URL_NAME, { params: { url } });
+	const { data } = await authClient.get(COMMON_ENDPOINT.GET_URL_NAME, { params: { url } });
 	return data;
 };
