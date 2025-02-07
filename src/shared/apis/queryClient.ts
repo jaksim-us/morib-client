0fb1be2ac46caff233fff85785919ae2b99ec908
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-07 16:52:57 +0900

    feat: queryClient 에러 던지지 않게 수정
    - error boundary가 아닌 react query 에러 범위 내에서 에러처리 예정

--- src/shared/apis/queryClient.ts
+++ src/shared/apis/queryClient.ts
@@ -1,12 +1,3 @@
 import { QueryClient } from '@tanstack/react-query';
 
-export const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: {
-			throwOnError: true,
-		},
-		mutations: {
-			throwOnError: true,
-		},
-	},
-});
+export const queryClient = new QueryClient();
