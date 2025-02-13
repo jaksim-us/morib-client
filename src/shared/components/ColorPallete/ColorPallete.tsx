50a14141264158adc9da2b85685c49c4f95d838f
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-13 22:16:48 +0900

    refactor: 컬레팔레트 컴포넌트 구조 변경 및 shared로 이동
    - 온보딩 뷰에서도 쓰이기 때문에 폴더 위치를 변경했습니다.

---
+++ src/shared/components/ColorPallete/ColorPallete.tsx
@@ -0,0 +1,38 @@
+import { ButtonHTMLAttributes, ReactNode, forwardRef } from 'react';
+
+import { COLOR_PALETTE_MAP } from '@/shared/constants/colorPalette';
+
+interface ColorPaletteRootProps {
+	isOpen: boolean;
+	children: ReactNode;
+}
+
+const ColorPaletteRoot = forwardRef<HTMLDivElement, ColorPaletteRootProps>(function ColorPaletteRoot(
+	{ isOpen, children },
+	ref,
+) {
+	if (!isOpen) return null;
+
+	return (
+		<div
+			ref={ref}
+			className="absolute left-0 top-[4.8rem] z-50 grid h-[11.6rem] w-[32rem] grid-cols-6 grid-rows-2 gap-x-[2rem] gap-y-[1.2rem] rounded-[8px] bg-gray-bg-04 px-[2rem] py-[2.2rem]"
+		>
+			{children}
+		</div>
+	);
+});
+
+interface ColorPaletteColorButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+	hashColor: keyof typeof COLOR_PALETTE_MAP;
+}
+
+const ColorPaletteColorButton = ({ hashColor, ...props }: ColorPaletteColorButtonProps) => {
+	return (
+		<button {...props} className={`h-[3rem] w-[3rem] flex-shrink-0 rounded-full ${COLOR_PALETTE_MAP[hashColor]}`} />
+	);
+};
+
+const ColorPalette = Object.assign(ColorPaletteRoot, { ColorButton: ColorPaletteColorButton });
+
+export default ColorPalette;
