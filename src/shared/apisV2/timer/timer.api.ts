918842005c73097b946525dc18cd708a4f1c66e0
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-25 15:21:57 +0900

    feat: timer api v2 추가

--- src/shared/apisV2/timer/timer.api.ts
+++ src/shared/apisV2/timer/timer.api.ts
@@ -1,4 +1,12 @@
-import { GetTimerTodosReq, GetTimerTodosRes, PostStopTimerReq, PostTimerStartReq } from '@/shared/types/api/timer';
+import {
+	GetPopoverAllowedServiceListRes,
+	GetTimerFriendsRes,
+	GetTimerTodosReq,
+	GetTimerTodosRes,
+	PostApplyAllowedServiceGroupReq,
+	PostStopTimerReq,
+	PostTimerStartReq,
+} from '@/shared/types/api/timer';
 
 import { authClient } from '../client';
 
@@ -7,6 +15,8 @@ const TIMER_ENDPOINT = {
 	GET_TIMER_FRIENDS: 'api/v2/timer/friends',
 	POST_STOP_TIMER: 'api/v2/timer/stop/:taskId',
 	POST_TIMER_START: 'api/v2/timer/run',
+	GET_POPOVER_ALLOWED_SERVICE_LIST: 'api/v2/timer/allowedGroups',
+	POST_APPLY_ALLOWED_SERVICE_GROUP: 'api/v2/timer/allowedGroups',
 };
 
 export const getTimerTodos = async ({ targetDate }: GetTimerTodosReq): Promise<GetTimerTodosRes> => {
@@ -14,7 +24,7 @@ export const getTimerTodos = async ({ targetDate }: GetTimerTodosReq): Promise<G
 	return data;
 };
 
-export const getTimerFriends = async (): Promise<GetTimerTodosRes> => {
+export const getTimerFriends = async (): Promise<GetTimerFriendsRes> => {
 	const { data } = await authClient.get(TIMER_ENDPOINT.GET_TIMER_FRIENDS);
 	return data;
 };
@@ -30,5 +40,15 @@ export const postStopTimer = async ({ taskId, elapsedTime, targetDate, runningCa
 
 export const postTimerStart = async ({ elapsedTime, runningCategoryName, taskId }: PostTimerStartReq) => {
 	const { data } = await authClient.post(TIMER_ENDPOINT.POST_TIMER_START, { elapsedTime, runningCategoryName, taskId });
+	return data;
+};
+
+export const getPopoverAllowedServiceList = async (): Promise<GetPopoverAllowedServiceListRes> => {
+	const { data } = await authClient.get(TIMER_ENDPOINT.GET_POPOVER_ALLOWED_SERVICE_LIST);
+	return data;
+};
+
+export const postApplyAllowedServiceGroup = async ({ allowedGroupIdList }: PostApplyAllowedServiceGroupReq) => {
+	const { data } = await authClient.post(TIMER_ENDPOINT.POST_APPLY_ALLOWED_SERVICE_GROUP, { allowedGroupIdList });
 	return data;
 };
