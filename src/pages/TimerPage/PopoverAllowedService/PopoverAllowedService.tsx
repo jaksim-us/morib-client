c6f317ee1ac3649790cefefcba97ace5542db146
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-25 15:33:04 +0900

    feat: 팝오버 컴포넌트 api 연동 및 체크박스 버튼 디자인 스펙에 맞게 변경

--- src/pages/TimerPage/PopoverAllowedService/PopoverAllowedService.tsx
+++ src/pages/TimerPage/PopoverAllowedService/PopoverAllowedService.tsx
@@ -1,64 +1,108 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-import { ALLOW_SITE_LIST } from '@/shared/constants/allowStieList';
+import type { PopoverAllowedSitesType } from '@/shared/types/allowedService';
 
-const PopoverAllowedService = ({
-	onCancel,
-	onRegister,
-}: {
-	onCancel: () => void;
-	onRegister: (selectedNames: string[]) => void;
-}) => {
-	const [checkedSetIds, setCheckedSetIds] = useState<number[]>([]);
-	const [selectedSetId, setSelectedSetId] = useState<number | null>(null);
+import { COLOR_PALETTE_MAP } from '@/shared/constants/colorPalette';
 
-	const handleCheckboxChange = (id: number) => {
-		setCheckedSetIds((prev) => (prev.includes(id) ? prev.filter((checkedId) => checkedId !== id) : [...prev, id]));
+import { getServiceFavicon } from '@/pages/OnboardingPage/utils/serviceUrl';
+import { usePostApplyAllowedServiceGroup } from '@/shared/apisV2/timer/timer.mutations';
+import { useGetPopoverAllowedServiceList } from '@/shared/apisV2/timer/timer.queries';
+
+import Checkbox from './Checkbox/Checkbox';
+
+const PopoverAllowedService = ({ onCancel }: { onCancel: () => void }) => {
+	const [selectedServiceGroupId, setSelectedServiceGroupId] = useState<number | null>(null);
+	const [checkedServiceGroupIds, setCheckedServiceGroupIds] = useState<number[]>([]);
+	const [activeAllowedSites, setActiveAllowedSites] = useState<PopoverAllowedSitesType>([]);
+
+	const { data: allowedServiceList } = useGetPopoverAllowedServiceList();
+	const { mutate: applyAllowedServiceGroup } = usePostApplyAllowedServiceGroup({
+		allowedGroupIdList: checkedServiceGroupIds,
+	});
+
+	const isCheckedServiceGroupId = (id: number) => checkedServiceGroupIds.includes(id);
+
+	const handleAllowedServiceGroupClick = (id: number) => {
+		setSelectedServiceGroupId(id);
+		changeActiveAllowedSites(id);
 	};
 
-	const handleSetClick = (id: number) => {
-		setSelectedSetId((prev) => (prev === id ? null : id));
+	const changeActiveAllowedSites = (selectedServiceGroupId: number) => {
+		const activeAllowedSites = allowedServiceList?.data?.find(
+			(group) => group.id === selectedServiceGroupId,
+		)?.allowedSites;
+
+		if (activeAllowedSites) {
+			setActiveAllowedSites(activeAllowedSites);
+		}
 	};
 
-	const handleRegisterClick = () => {
-		const selectedNames = ALLOW_SITE_LIST.filter((item) => checkedSetIds.includes(item.id)).map(
-			(item) => item.allowedservicesetname,
-		);
-		onRegister(selectedNames);
+	const toggleCheckedServiceGroupIds = (id: number) => {
+		if (isCheckedServiceGroupId(id)) {
+			const newActiveServiceGroupIds = checkedServiceGroupIds.filter((checkedId) => checkedId !== id);
+			setCheckedServiceGroupIds(newActiveServiceGroupIds);
+		} else {
+			const newActiveServiceGroupIds = [...checkedServiceGroupIds, id];
+			setCheckedServiceGroupIds(newActiveServiceGroupIds);
+		}
 	};
+
+	const handleApplyAllowedServiceGroup = () => {
+		applyAllowedServiceGroup(undefined, {
+			onSuccess: () => {
+				onCancel();
+			},
+		});
+	};
+
+	useEffect(() => {
+		if (allowedServiceList) {
+			const registeredNames = [] as string[];
+
+			allowedServiceList.data.forEach((group) => {
+				if (group.selected) {
+					if (!isCheckedServiceGroupId(group.id)) {
+						setCheckedServiceGroupIds((prev) => [...prev, group.id]);
+					}
+
+					registeredNames.push(...group.allowedSites.map((site) => site.siteName));
+				}
+			});
+		}
+	}, [allowedServiceList]);
 
 	return (
 		<div className="flex h-[45rem] w-[53.2rem]">
 			<div className="flex w-[23rem] flex-col rounded-l-[8px] bg-gray-bg-02 p-[1rem]">
 				<h3 className="h-[3.4rem] w-[21rem] px-[1.7rem] py-[1rem] text-gray-04 detail-reg-14">허용서비스 세트</h3>
 				<ul className="mt-[0.8rem] h-[32.4rem] w-[20.4rem] overflow-y-auto">
-					{ALLOW_SITE_LIST.map((item) => (
+					{allowedServiceList?.data?.map((service) => (
 						<li
-							key={item.id}
-							className={`flex h-[3.4rem] w-[20.4rem] cursor-pointer items-center rounded-[6px] py-[0.3rem] pl-[1rem] pr-[0.3rem] hover:bg-gray-bg-04 ${
-								item.id === selectedSetId ? 'bg-gray-bg-05' : ''
+							key={service.id}
+							className={`flex h-[3.4rem] w-[20.4rem] cursor-pointer items-center rounded-[6px] py-[0.3rem] pl-[1rem] pr-[0.3rem] hover:bg-gray-bg-04 focus:outline-none ${
+								service.id === selectedServiceGroupId ? 'bg-gray-bg-05' : ''
 							}`}
-							onClick={() => handleSetClick(item.id)}
+							onClick={() => handleAllowedServiceGroupClick(service.id)}
+							tabIndex={0}
 						>
 							<div
-								className="h-[2.8rem] w-[2.8rem] p-[0.7rem]"
 								onClick={(e) => {
 									e.stopPropagation();
 								}}
+								className="h-[2.8rem] w-[2.8rem] p-[0.7rem]"
 							>
-								<input
-									type="checkbox"
-									id={`${item.id}`}
-									checked={checkedSetIds.includes(item.id)}
-									onChange={() => handleCheckboxChange(item.id)}
-									className="h-[1.4rem] w-[1.4rem] rounded border-gray-600 focus:ring"
+								<Checkbox
+									onClick={() => {
+										toggleCheckedServiceGroupIds(service.id);
+									}}
+									checked={isCheckedServiceGroupId(service.id)}
 								/>
 							</div>
 							<div className="w-[14.9rem] cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap text-white detail-semibold-14">
-								{item.allowedservicesetname}
+								{service.name}
 							</div>
 
-							<span className="h-[1rem] w-[1rem] rounded-full bg-yellow-500" />
+							<span className={`h-[1rem] w-[1rem] rounded-full ${COLOR_PALETTE_MAP[service.colorCode]}`} />
 						</li>
 					))}
 				</ul>
@@ -70,7 +114,7 @@ const PopoverAllowedService = ({
 						취소
 					</button>
 					<button
-						onClick={handleRegisterClick}
+						onClick={handleApplyAllowedServiceGroup}
 						className="flex h-[3.4rem] w-[5.8rem] items-center justify-center rounded-[3.297px] bg-mint-02 text-black detail-semibold-14"
 					>
 						등록
@@ -81,17 +125,15 @@ const PopoverAllowedService = ({
 			<div className="flex w-[30.2rem] flex-col gap-[0.8rem] rounded-r-[8px] bg-gray-bg-03 py-[1rem] pl-[0.9rem] pr-[1.7rem]">
 				<h3 className="h-[3.4rem] w-[28.6rem] p-[1rem] text-gray-04 detail-reg-14">허용할 사이트</h3>
 				<ul className="h-[37.6rem] overflow-y-auto">
-					{ALLOW_SITE_LIST.filter((item) => item.id === selectedSetId).map((item) =>
-						item.allowedsites.map((site, index) => (
-							<li
-								key={index}
-								className="flex h-[3.2rem] w-[27.6rem] items-center gap-[1rem] rounded-[3px] px-[0.7rem] odd:bg-gray-bg-02"
-							>
-								<img src={site.src} alt={`${site.name} Icon`} className="h-6 w-6" />
-								<span className="text-white detail-reg-12">{site.name}</span>
-							</li>
-						)),
-					)}
+					{activeAllowedSites.map((siteInfo) => (
+						<li
+							key={siteInfo.id}
+							className="flex h-[3.2rem] w-[27.6rem] items-center gap-[1rem] rounded-[3px] px-[0.7rem] odd:bg-gray-bg-02"
+						>
+							<img src={getServiceFavicon(siteInfo.siteUrl)} alt={`${siteInfo.siteName} Icon`} className="h-6 w-6" />
+							<span className="truncate text-white detail-reg-12">{siteInfo.siteName}</span>
+						</li>
+					))}
 				</ul>
 			</div>
 		</div>
