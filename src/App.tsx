237cb77211a6520a6662af7d4ba908033a146d3b
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-25 15:22:26 +0900

    chore: global error boundary 삭제 (react-query로만 에러 잡을 예정)

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { queryClient } from '@/shared/apis/queryClient';
 
 import router from './router/Router';
-import GlobalErrorBoundary from './shared/components/ErrorBoundary/GlobalErrorBoundary';
 
 const App = () => {
 	return (
@@ -17,9 +16,7 @@ const App = () => {
 				<ReactQueryDevtools initialIsOpen={false} />
 			</div>
 			<Provider>
-				<GlobalErrorBoundary>
-					<RouterProvider router={router} />
-				</GlobalErrorBoundary>
+				<RouterProvider router={router} />
 			</Provider>
 		</QueryClientProvider>
 	);
