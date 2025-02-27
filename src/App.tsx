1ae04240e401bdef3f53f51f51c3c562ce7553b7
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-28 00:51:29 +0900

    chore: queryClient v2로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
-import { queryClient } from '@/shared/apis/queryClient';
+import { queryClient } from '@/shared/apisV2/queryClient';
 
 import router from './router/Router';
 
