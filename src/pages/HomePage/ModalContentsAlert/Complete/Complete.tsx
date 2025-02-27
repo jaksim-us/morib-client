e89914078c6b074ed446d414559a1a958f1d23ad
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-28 01:03:42 +0900

    feat: prop 변경 누락된 부분 수정

--- src/pages/HomePage/ModalContentsAlert/Complete/Complete.tsx
+++ src/pages/HomePage/ModalContentsAlert/Complete/Complete.tsx
@@ -1,12 +1,12 @@
 import ButtonAlert from '../ButtonAlert/ButtonAlert';
 import { AlertModalProps } from '../types/index';
 
-const Complete = ({ handleClose, userEmail }: AlertModalProps) => (
+const Complete = ({ onCloseModal, userEmail }: AlertModalProps) => (
 	<div className="flex flex-col rounded-[0.8rem] bg-gray-bg-04 p-[3rem]">
 		<div className="w-[47.2rem]">
 			<p className="flex justify-center text-white subhead-bold-22">{userEmail} 계정이</p>
 			<p className="mb-[3rem] flex justify-center text-white subhead-bold-22"> 삭제되었습니다.</p>
-			<ButtonAlert variant="primary" onClick={handleClose}>
+			<ButtonAlert variant="primary" onClick={onCloseModal}>
 				확인
 			</ButtonAlert>
 		</div>
