b0733cde9b447210d4f6ccef32da7a633d78c2dc
suwonthugger <127329855+suwonthugger@users.noreply.github.com>
2025-02-28 00:53:52 +0900

    feat: SSE 재연결 데이터 형식 쿼리 스트링으로 수정

--- src/shared/apisV2/SSE/SSE.endpoint.ts
+++ src/shared/apisV2/SSE/SSE.endpoint.ts
@@ -1,4 +1,13 @@
 export const SSE_ENDPOINT = {
 	GET_SSE_CONNECTION: 'api/v2/sse/connect',
-	GET_SSE_REFRESH: 'api/v2/sse/refresh',
+	GET_SSE_REFRESH: ({
+		elapsedTime,
+		runningCategoryName,
+		taskId,
+	}: {
+		elapsedTime?: number;
+		runningCategoryName?: string;
+		taskId?: number;
+	}) =>
+		`api/v2/sse/refresh${elapsedTime && `?elapsedTime=${elapsedTime}`}${runningCategoryName && `?runningCategoryName=${runningCategoryName}`}${taskId && `?taskId=${taskId}`}`,
 };
